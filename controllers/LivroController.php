ff3888f9d7fa2149a3f342ac25433e64c62007fa
bruna <bruna.ssantos102@senacsp.edu.br>
2023-11-17 14:20:52 -0300

    17-11

--- controllers/LivroController.php
+++ controllers/LivroController.php
@@ -41,7 +41,7 @@ class LivroController
 
                 $pastaDestino = $uploadDir. $novoNomeArquivo;
 
-                move_uploaded_file($_FILES ['capa']['tmp_name'],$pastaDestino);
+                 move_uploaded_file($_FILES ['capa']['tmp_name'],$pastaDestino);
 
                 $dados['capa'] = $novoNomeArquivo;
 
@@ -55,6 +55,7 @@ class LivroController
     public function editarLivro()
     {
         $id_livro = $_GET['id_livro'];
+        $livro = $this->livroModel->buscar($id_livro);
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
             $dados = [
@@ -65,17 +66,41 @@ class LivroController
                 'ano_publicacao' => $_POST['ano_publicacao'],
                 'isbn' => $_POST['isbn']
             ];
+            if(isset($_FILES['capa']['name']) && !empty($_FILES['capa']['name']))  {
+                $fileInfo = pathinfo($_FILES['capa']['name']);
+                $nomeArquivo = md5(uniqid());
+                $uploadDir = __dir__."/../uploads/";
+
+                if(!is_dir($uploadDir)) {
+                    mkdir($uploadDir,0777, true);
+                }
+
+                $novoNomeArquivo = $nomeArquivo . "." . $fileInfo["extension"];
+
+                $pastaDestino = $uploadDir. $novoNomeArquivo;
+
+                 move_uploaded_file($_FILES ['capa']['tmp_name'],$pastaDestino);
+
+                $dados['capa'] = $novoNomeArquivo;
+
+            }else{ 
+                $dados['capa'] = $livro->capa;
+            }
 
             $this->livroModel->editar($id_livro, $dados);
             header('Location: index.php');
             exit;
         }
 
-        return $this->livroModel->buscar($id_livro);
+        return $livro ;
     }
 
     public function excluirLivro()
     {
+        $livro = $this->livroModel->buscar($_GET['id_livro']);
+        $imagemcapa = __DIR__."/../uploads/".$livro->capa;
+        unset($imagemcapa);
+        
         $this->livroModel->excluir($_GET['id_livro']);
         header('Location: index.php');
         exit;
